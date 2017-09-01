417fc523e306074167c0f5d2eada347baee631ab
Son Hoang <sonhv82@gmail.com>
2017-09-02 05:12:44 +0700

    3rd

--- apps/medzam/loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$url = $_GET['url'];
-	//$url = "https://mygrubbook.com/control/static/compress/medzam.html";
-	$data = file_get_contents($url);
-	echo $data;
-?>
--- apps/medzam/loader_manifest.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$url = "http://cstsolution.com/clients/medzam/apps/install/manifest.plist";
-	$data = file_get_contents($url);
-	echo $data;
-?>
